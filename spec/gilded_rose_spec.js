78f20fcd66f15e43640dfc2c5f827c9d617d31d2
Keith Magill <keithmagill1@gmail.com>
2016-10-06 17:10:48 +0100

    Initial checkin

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -10,19 +10,19 @@ describe("Gilded Rose", function() {
     describe("Adding a test item to the array", () => {
         items = []
         items.push(new Item('Test Item', 1, 10))
-        it("Pass in a test item and expect array length to increase by one", () => {
+        it("Add a standard item and expect array length to increase by one", () => {
             expect(items.length).toEqual(1)
         })
-        it("Expect test item quality to decrease by 1", () => {
+        it("Expect standard item quality to decrease by 1", () => {
             update_quality()
             expect(items[0].quality).toEqual(9)
             expect(items[0].sell_in).toEqual(0)
         })
-        it("Expect test item sell_in value to decrease by 1", () => {
+        it("Expect standard item sell_in value to decrease by 1", () => {
             update_quality()
             expect(items[0].sell_in).toEqual(-1)
         })
-        it("Expect test item qualiy to be decreased by two as sell_in days is less than zero", () => {
+        it("Expect standard item qualiy to be decreased by two as sell_in days is less than zero", () => {
             update_quality()
             expect(items[0].quality).toEqual(5)
         })
@@ -34,28 +34,28 @@ describe("Gilded Rose", function() {
         })
     })
     describe("Test more complex rules - Aged Brie", () => {
-        it("Test Aged Brie increases in quality as sell_in decreases", () => {
+        it("Aged Brie increases in quality as sell_in decreases", () => {
             items = []
             items.push(new Item('Aged Brie', 2, 0))
             update_quality()
             expect(items[0].sell_in).toEqual(1)
             expect(items[0].quality).toEqual(1)
         })
-        it("Test that Aged Brie quality does not increase above 50", () => {
+        it("Aged Brie quality should not increase above 50", () => {
             items = []
             items.push(new Item("Aged Brie", 2, 50))
             update_quality()
             expect(items[0].quality).not.toEqual(51)
         })
-        it('Test that when Aged brie is less than 0m days quality should increase by 1', () => {
+        it('When Aged brie is less than 0 days quality will increase by 2', () => {
             items = []
             items.push(new Item("Aged Brie", 0, 10))
             update_quality()
             expect(items[0].quality).toEqual(11)
         })
     })
-    describe("test more complex rules - Sulfuras", () => {
-        it("Test no change in sell_in or quality of Sulfuras", () => {
+    describe("Test more complex rules - Sulfuras", () => {
+        it("Test no change in sell in days or quality of Sulfuras", () => {
             items = []
             items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
             expect(items[0].name).toEqual("Sulfuras, Hand of Ragnaros")
@@ -64,19 +64,19 @@ describe("Gilded Rose", function() {
         })
     })
     describe("Test more complex rules - Backstage Pass", () => {
-        it("When days_in is 10 or less but above 5 quality shold increase by 2", () => {
+        it("When sell in days is 10 or less but above 5 quality shold increase by 2", () => {
             items = []
             items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 6, 10))
             update_quality()
             expect(items[0].sell_in).toEqual(5)
             expect(items[0].quality).toEqual(12)
         })
-        it("When days is 5 or less and above 0 quality should increase by 3", () => {
+        it("When sell in days is 5 or less and above 0 quality should increase by 3", () => {
             update_quality()
             expect(items[0].sell_in).toEqual(4)
             expect(items[0].quality).toEqual(15)
         })
-        it("When days is greater than 10 then qaulity should increase by 1", () => {
+        it("When sell in days is greater than 10 then qaulity should increase by 1", () => {
             items = []
             items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 25, 49))
             update_quality()
@@ -88,12 +88,71 @@ describe("Gilded Rose", function() {
             expect(items[0].sell_in).toEqual(23)
             expect(items[0].quality).toEqual(50)
         })
-        it("When days is 0 or below then quality should be set to 0", () => {
+        it("When sell in days is 0 or below then quality should be set to 0", () => {
             items = []
             items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 30))
             update_quality()
             expect(items[0].sell_in).toEqual(-1)
             expect(items[0].quality).toEqual(0)
         })
+
+    })
+    describe("Multipe array items", () => {
+        it('Tests that sell_in days and quality is updated if multiple items are provided', () => {
+            items = []
+            items.push(new Item('Test Item', 1, 10))
+            items.push(new Item("Aged Brie", 2, 30))
+            update_quality()
+            expect(items[0].sell_in).toEqual(0)
+            expect(items[0].quality).toEqual(9)
+            expect(items[1].sell_in).toEqual(1)
+            expect(items[1].quality).toEqual(31)
+        })
+        it('Tests that item 0 will not go below quality of 0, item 1 Brie will not go above 50 quality', () => {
+            items = []
+            items.push(new Item('Test Item', 5, 0))
+            items.push(new Item('Aged Brie', 6, 50))
+            update_quality()
+            expect(items[0].sell_in).toEqual(4)
+            expect(items[0].quality).toEqual(0)
+            expect(items[1].sell_in).toEqual(5)
+            expect(items[1].quality).toEqual(50)
+
+        })
+    })
+    describe("Test more complex rules - Conjured ", () => {
+        it('At the end of each day the quality of Conjured decreases by 2', () => {
+            items = []
+            items.push(new Item("Conjured Mana Cake", 10, 20))
+            update_quality()
+            expect(items[0].name).toEqual("Conjured Mana Cake")
+            expect(items[0].sell_in).toEqual(9)
+            expect(items[0].quality).toEqual(18)
+        })
+        it('For Conjured quality will not decrese below 0', () => {
+            items = []
+            items.push(new Item("Conjured Mana Cake", 10, 1))
+            update_quality()
+            expect(items[0].name).toEqual("Conjured Mana Cake")
+            expect(items[0].sell_in).toEqual(9)
+            expect(items[0].quality).toEqual(0)
+        })
+        it('When sell in days is less than 0 then quality of Conjured should decrease by 4', () => {
+            items = []
+            items.push(new Item("Conjured Mana Cake", -1, 15))
+            update_quality()
+            expect(items[0].name).toEqual("Conjured Mana Cake")
+            expect(items[0].sell_in).toEqual(-2)
+            expect(items[0].quality).toEqual(11)
+        })
+        it("When sell in days is less than 0 then quality will be set to 0", () => {
+            items = []
+            items.push(new Item("Conjured Mana Cake", 0, 3))
+            update_quality()
+            expect(items[0].name).toEqual("Conjured Mana Cake")
+            expect(items[0].sell_in).toEqual(-1)
+            expect(items[0].quality).toEqual(0)
+        })
+
     })
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,7 +1,7 @@
 function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
 }
 
 var items = []
@@ -14,49 +14,50 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+    for (var i = 0; i < items.length; i++) {
+
+        switch (items[i].name) {
+            case 'Aged Brie':
+                agedBrie(i)
+                break;
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                backStage(i)
+                break;
+            case 'Sulfuras, Hand of Ragnaros':
+                break;
+            case 'Conjured Mana Cake':
+                conjured(i)
+                break;
+            default:
+                standard(i)
         }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+}
+
+function standard(i) {
+    if (items[i].sell_in > 0 && items[i].quality > 0) items[i].quality -= 1
+    if (items[i].sell_in <= 0 && items[i].quality > 0) items[i].quality -= 2
+    items[i].sell_in -= 1
+}
+
+function agedBrie(i) {
+    if (items[i].quality < 50) items[i].quality += 1
+    items[i].sell_in -= 1
+        //this is the strange line of code
+    if (items[i].sell_in <= 0) items[i].quality += 1
+}
+
+function backStage(i) {
+    if (items[i].quality < 50) items[i].quality += 1
+    if (items[i].sell_in < 11 && items[i].quality < 50) items[i].quality += 1
+    if (items[i].sell_in < 6 && items[i].quality < 50) items[i].quality += 1
+    items[i].sell_in -= 1
+    if (items[i].sell_in <= 0) items[i].quality = 0
+}
+
+function conjured(i) {
+    if (items[i].sell_in > 0 && items[i].quality > 0) items[i].quality -= 2
+    if (items[i].sell_in <= 0 && items[i].quality > 0) items[i].quality -= 4
+    items[i].sell_in -= 1
+    if (items[i].quality < 0) items[i].quality = 0
 }
